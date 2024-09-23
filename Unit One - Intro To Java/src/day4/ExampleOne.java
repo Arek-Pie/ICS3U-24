a44243968438ff6aebe34594cd1cc28afb6dec17
Eric Pei <spei@bayviewglen.ca>
2024-09-23 10:28:04 -0400

    day 4

---
+++ Unit One - Intro To Java/src/day4/ExampleOne.java
@@ -0,0 +1,13 @@
+package day4;
+
+public class ExampleOne 
+{
+    public static void main(String[] args) 
+    {
+        int x = 7, y = 9;
+
+        System.out.println(x + y);
+        System.out.println(x + "+" + y + "=" + x + y);
+        System.out.println(x + "+" + y + "=" + (x + y));
+    }
+}
